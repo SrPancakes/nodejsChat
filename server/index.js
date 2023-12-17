92c7d041b7f8ea6b85a1ce6f971a9d1be98c1de0
SrPancakes <adormilucasnunez@gmail.com>
2023-12-17 16:50:00 +0100

    socket code ready

--- server/index.js
+++ server/index.js
@@ -1,15 +1,23 @@
 import express from 'express';
 import logger from 'morgan';
 
+import { Server } from 'socket.io';
+import { createServer } from 'http';
+
 const port = process.env.PORT ?? 3000;
 
 const app = express()
-app.use(logger('dev'))
+const server = createServer(app)
+const io = new Server(server)
+
+io.on('connection', () => {
+  console.log('a user has connected!')
+})
 
 app.get('/', (req, res) => {
   res.sendFile(process.cwd() + '/client/index.html')
 })
 
-app.listen(port, () => {
+server.listen(port, () => {
   console.log(`Server is listening on port ${port}`)
 })
