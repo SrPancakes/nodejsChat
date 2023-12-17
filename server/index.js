98f1e737875734fe409ae661ccd1b78d117bc3fc
SrPancakes <adormilucasnunez@gmail.com>
2023-12-17 18:19:20 +0100

    main app functioning, working on users

--- server/index.js
+++ server/index.js
@@ -1,18 +1,73 @@
 import express from 'express';
 import logger from 'morgan';
+import dotenv from 'dotenv';
+import { createClient } from '@libsql/client';
 
 import { Server } from 'socket.io';
 import { createServer } from 'http';
 
+dotenv.config()
 const port = process.env.PORT ?? 3000;
 
 const app = express()
 const server = createServer(app)
-const io = new Server(server)
-
-io.on('connection', () => {
-  console.log('a user has connected!')
+const io = new Server(server, {
+  connectionStateRecovery: {}
 })
+
+const db = createClient({
+  url: 'libsql://node-chat-srpancakes.turso.io',
+  authToken: process.env.DB_TOKEN
+})
+
+await db.execute(`
+  CREATE TABLE IF NOT EXISTS messages (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    content TEXT
+  )
+`)
+
+io.on('connection', async (socket) => {
+  console.log('a user has connected!')
+
+  socket.on('disconnect', () => {
+    console.log('a user has disconnected')
+  })
+
+  socket.on('chat message', async (msg) => {
+    let result
+    try {
+      result = await db.execute({
+        sql: 'INSERT INTO messages (content) VALUES (:msg)',
+        args: { msg }
+      })
+    } catch (err) {
+      console.error(err)
+      return
+    }
+
+    io.emit('chat message', msg, result.lastInsertRowid.toString())
+  })
+
+  if (!socket.recovered) {
+    try {
+      const results = await db.execute({
+        sql: 'SELECT * FROM messages WHERE id > ?',
+        args: [socket.handshake.auth.serverOffset ?? 0]
+      })
+
+      results.rows.forEach(row => {
+        socket.emit('chat message', row.content, row.id.toString())
+      })
+    } catch (err) {
+      console.error(err)
+    }
+  }
+
+})
+
+
+app.use(logger('dev'))
 
 app.get('/', (req, res) => {
   res.sendFile(process.cwd() + '/client/index.html')
